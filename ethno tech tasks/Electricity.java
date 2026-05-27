01c3b6f688dadb6cb251756b4cc4a578fdda1fc6
poojithathupalli88 <poojithathupalli@gmail.com>
2026-05-27 11:20:34 +0530

    Update Electricity.java

--- ethno tech tasks/Electricity.java
+++ ethno tech tasks/Electricity.java
@@ -1,1 +1,25 @@
-w
+import java.util.*;
+class Electricity{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Electricity bill:");
+    int units=sc.nextInt();
+    int bill=0;
+    if(units<=200){
+      bill=0;
+    }
+    else if(units>200 && units<=300)
+    {
+      bill=50;
+    }
+    else if(units>1000){
+      bill=200;
+    }
+    else{
+      bill=300;
+    }
+    System.out.println("Electricity bill:"+ bill);
+    sc.close();
+  }
+}
+    
